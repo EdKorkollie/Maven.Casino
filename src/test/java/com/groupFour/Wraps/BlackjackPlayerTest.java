a116ed8d96e73dce31f94bf9ca224a3b3daaf314
Edward Korkollie <korkollieed@gmail.com>
2019-06-11 18:06:35 -0400

    Added BlackjackPlayerTest class.

---
+++ src/test/java/com/groupFour/Wraps/BlackjackPlayerTest.java
@@ -0,0 +1,24 @@
+package com.groupFour.Wraps;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlackjackPlayerTest {
+
+    @Test
+    public void getBalance() {
+    }
+
+    @Test
+    public void setBalance() {
+    }
+
+    @Test
+    public void addToBalance() {
+    }
+
+    @Test
+    public void subtractFromBalance() {
+    }
+}
